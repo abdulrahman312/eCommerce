b7003948d6b585fbac0e0d8d06edf4ca761a1edd
Abdul Rahman Khan <abdulrahman312@yahoo.com>
2021-07-27 16:49:37 +0300

    ShopPage view added will CollectionPreview and CollectionItem components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/HomePage';
-import { Route, Switch, Link } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import ShopPage from './pages/shop/ShopPage';
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
 
     </div>
   );
 }
+
+
 
 
 
